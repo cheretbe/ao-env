ebcb0da27ca0a3e5c633aeb7b42af8edb860b025
cheretbe <a.a.orlov@gmail.com>
2020-04-26 14:48:48 +0200

    Add Dockert containers to run tests

--- tests/tasks.py
+++ tests/tasks.py
@@ -6,7 +6,7 @@ def run_docker_test(context, image):
         context.run(
             f"docker build . --build-arg base_image={image} --tag test-image:1.0 -f tests/docker/Dockerfile"
         )
-    context.run("docker run --rm --name test test-image:1.0 /ao-env/install.py")
+    context.run("docker run --rm --name test test-image:1.0 su - testuser -c /ao-env/install.sh")
     # context.run("docker run --rm --name test test-image:1.0 /bin/sh -c 'ls -lha'")
 
 @invoke.task(default=True)
@@ -22,3 +22,16 @@ def test_ubuntu_bionic(context):
 @invoke.task(test_ubuntu_bionic)
 def test(context):
     """Run all tests"""
+
+@invoke.task
+def interactive(context):
+    """Run interactive bash shell on a temporary Ubuntu 18.04 container"""
+    with context.cd(".."):
+        context.run(
+            f"docker build . --build-arg base_image=ubuntu:18.04 --tag test-image:1.0 -f tests/docker/Dockerfile"
+        )
+    context.run(
+        "docker run --rm  -ti -v /ao-env:/ao-env:ro --name test"
+        " test-image:1.0 su - testuser -c /bin/bash",
+        pty=True
+    )
