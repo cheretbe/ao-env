f7f282ef62c92e4894888c35fb0696e657a35e34
cheretbe <a.a.orlov@gmail.com>
2020-04-28 10:52:19 +0200

    Lint code

--- bin/vagrant-box-update.py
+++ bin/vagrant-box-update.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# pylint: disable=invalid-name,missing-module-docstring,missing-function-docstring
+
 import sys
 import types
 import subprocess
@@ -9,8 +11,11 @@ import humanfriendly.prompts
 
 def run_command(command):
     result = []
-    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE,
-        universal_newlines=True)
+    process = subprocess.Popen(
+        shlex.split(command),
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
     while True:
         output = process.stdout.readline().strip()
         if output == "" and process.poll() is not None:
@@ -26,17 +31,18 @@ def run_command(command):
 boxes_to_update = []
 
 
-# for line in ["* 'ubuntu/xenial64' for 'virtualbox' is outdated! Current: 20200320.0.0. Latest: 20200415.0.0"]:
 for line in run_command("vagrant box outdated --global"):
     if "is outdated!" in line:
+        # pylint: disable=line-too-long
         # We are looking for lines like this:
         # * 'ubuntu/xenial64' for 'virtualbox' is outdated! Current: 20200320.0.0. Latest: 20200415.0.0
+        # pylint: enable=line-too-long
         tokens = line.split()
         boxes_to_update += [
             types.SimpleNamespace(
-                    name=tokens[1].replace("'", ""),
-                    old_version=tokens[7],
-                    new_version=tokens[9]
+                name=tokens[1].replace("'", ""),
+                old_version=tokens[7],
+                new_version=tokens[9]
             )
         ]
 
