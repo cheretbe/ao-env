602c5601eb82992edbed1566980d80da904f08cf
cheretbe <a.a.orlov@gmail.com>
2020-05-02 22:44:05 +0200

    Finish the Vagrant box publishing script

--- bin/vagrant_box_publish.py
+++ bin/vagrant_box_publish.py
@@ -5,10 +5,25 @@
 import sys
 import argparse
 import datetime
+import pathlib
 import subprocess
 import packaging
 import requests
 import PyInquirer
+
+
+def ask_for_confirmation(prompt):
+    conf_questions = [
+        {
+            "type": "confirm",
+            "name": "continue",
+            "message": prompt,
+            "default": True
+        }
+    ]
+    conf_answers = PyInquirer.prompt(conf_questions)
+    return bool(conf_answers) and conf_answers["continue"]
+
 
 def inc_version_release(new_base_version, current_version, separator):
     current_base_version, current_subversion = current_version.split(separator)
@@ -23,8 +38,41 @@ def inc_version_release(new_base_version, current_version, separator):
         )
     return new_version
 
+
+def select_box_file():
+    box_files = sorted(pathlib.Path().rglob("*.box"))
+    if len(box_files) == 0:
+        raise Exception(
+            "Can't find any *.box files in current directory and subdirectories"
+        )
+    if len(box_files) == 1:
+        return box_files[0]
+    answers = PyInquirer.prompt(
+        questions=[
+            {
+                'type': 'list',
+                'name': 'boxfile',
+                'message': 'Select a box file to publish',
+                'choices': [str(i) for i in box_files]
+            }
+        ]
+    )
+    if not answers:
+        sys.exit(1)
+    return answers["boxfile"]
+
+
 def main():
-    box_names = ["docker-ce", "dummy"]
+    box_data = [
+        {
+            "name": "docker-ce",
+            "description": "Ubuntu 18.04 with Docker CE and testing tools installed"
+        },
+        {
+            "name": "dummy",
+            "description": "Temporary box for upload testing"
+        }
+    ]
 
     parser = argparse.ArgumentParser(description="Publish boxes to Vagrant Cloud")
     parser.add_argument(
@@ -33,6 +81,11 @@ def main():
         default="",
         help="Box base version (optional, yyyymmdd will be used by default, where yyyy "
              "is current year, mm is current month, dd is current day)"
+    )
+    parser.add_argument(
+        "-f", "--box-file",
+        default="",
+        help="Path to a box file to publish"
     )
     parser.add_argument(
         "-s", "--version-separator",
@@ -46,45 +99,49 @@ def main():
         options.box_ver = datetime.datetime.now().strftime("%Y%m%d")
         print(f"Box version is not specified. Using {options.box_ver}")
 
+    if options.box_file == "":
+        options.box_file = select_box_file()
+    print(f"Box file name: {options.box_file}")
+
     print("Checking Vagrant Cloud login...")
 
-    # if subprocess.call(
-    #         "vagrant cloud auth login --check",
-    #         shell=True,
-    #         stdin=subprocess.DEVNULL,
-    #         stdout=subprocess.DEVNULL
-    # ) != 0:
-    #     print("You are not currently logged in.")
-    #     print("Please provide your login information to authenticate.")
-    #     subprocess.check_call("vagrant cloud auth login", shell=True)
+    if subprocess.call(
+            "vagrant cloud auth login --check",
+            shell=True,
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL
+    ) != 0:
+        print("You are not currently logged in.")
+        print("Please provide your login information to authenticate.")
+        subprocess.check_call("vagrant cloud auth login", shell=True)
 
-    # cloud_user_name = ""
-    # for line in subprocess.check_output(
-    #         "vagrant cloud auth whoami",
-    #         shell=True,
-    #         universal_newlines=True
-    # ).splitlines():
-    #     print(line)
-    #     if "Currently logged in as" in line:
-    #         cloud_user_name = line.split(" ")[-1]
+    cloud_user_name = ""
+    for line in subprocess.check_output(
+            "vagrant cloud auth whoami",
+            shell=True,
+            universal_newlines=True
+    ).splitlines():
+        print(line)
+        if "Currently logged in as" in line:
+            cloud_user_name = line.split(" ")[-1]
 
-    # if not cloud_user_name:
-    #     raise Exception("Could not detect current Vagrant Cloud user name")
-    cloud_user_name = "dummy"
+    if not cloud_user_name:
+        raise Exception("Could not detect current Vagrant Cloud user name")
 
     answers = PyInquirer.prompt(
         questions=[
             {
                 'type': 'list',
                 'name': 'box',
-                'message': 'Select a box to publish',
-                'choices': box_names
+                'message': 'Select a name to publish a box under',
+                'choices': [i["name"] for i in box_data]
             }
         ]
     )
     if not answers:
         sys.exit(1)
     box_name = answers["box"]
+    box_description = next(i for i in box_data if i["name"] == box_name)["description"]
 
     print(f"'{cloud_user_name}/{box_name}' is selected")
 
@@ -103,10 +160,35 @@ def main():
         print(f"There is no currently released version of '{cloud_user_name}/{box_name}'")
         new_version = options.box_ver + options.version_separator + "0"
 
-    print(new_version)
+    if not ask_for_confirmation(
+            f"Do you want to release '{options.box_file}' as '{cloud_user_name}/{box_name}' "
+            f"version {new_version}?"
+    ):
+        sys.exit("Cancelled by user")
 
-    # if not ask_for_confirmation("Do you want to release version '{}' of the box?".format(new_version)):
-    #     sys.exit(1)
+    answers = PyInquirer.prompt(
+        questions=[
+            {
+                "type": "editor",
+                "name": "description",
+                "message": "Please enter a version description (Alt+Enter to finish)",
+                "default": box_description
+            }
+        ]
+    )
+    if not answers:
+        sys.exit(1)
+    version_description = answers["description"]
+
+    print(f"Publishing '{options.box_file}' as '{cloud_user_name}/{box_name}'version {new_version}")
+    subprocess.check_call(
+        f"vagrant cloud publish '{cloud_user_name}/{box_name}' "
+        f"{new_version} virtualbox {options.box_file} "
+        f"--short-description '{box_description}' "
+        f"--version-description '{version_description}' "
+        "--release --force",
+        shell=True
+    )
 
 
 if __name__ == '__main__':
