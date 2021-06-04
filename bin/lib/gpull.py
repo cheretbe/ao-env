d7fac8b4aa0cd82e559d38200b43b512080c039d
cheretbe <a.a.orlov@gmail.com>
2021-06-04 10:46:10 +0200

    Add safe git pull wrapper script

---
+++ bin/lib/gpull.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+def main():
+    if subprocess.check_output(["git", "status", "--porcelain"]) == b"":
+        if subprocess.check_output(["git", "diff", "@{u}"]) == b"":
+            subprocess.check_call(["git", "pull"])
+        else:
+            sys.exit("Will not pull as local Git repo has unpushed commits")
+    else:
+        sys.exit("Will not pull as local Git repo has uncommitted changes")
+
+if __name__ == "__main__":
+    main()
