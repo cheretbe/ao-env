abad37fe63d46961f0b9e73938bad439e5e588b0
cheretbe <a.a.orlov@gmail.com>
2021-06-07 14:56:50 +0200

    Add ConEmu

--- windows/install.ps1
+++ windows/install.ps1
@@ -2,11 +2,36 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 
 $script:scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$autoexecPath = Join-Path -Path $script:scriptDir -ChildPath "ao-env_autoexec.bat"
 
+function CreateShortcut {
+param(
+  [string]$shortcutFileName,
+  [string]$shortcutTarget,
+  [string]$arguments,
+  [string]$iconLocation
+)
+  $wsShell = New-Object -comObject "WScript.Shell"
+  $tcShortcut = $wsShell.CreateShortcut($shortcutFileName)
+  $tcShortcut.TargetPath = $shortcutTarget
+  if ($arguments)
+    { $tcShortcut.Arguments = $arguments }
+  if ($iconLocation)
+    { $tcShortcut.IconLocation = $iconLocation }
+  if ($shortcutFileName.contains(".url")) {
+    $tcShortcut.Save()
+  } else {
+    if (Test-Path -Path $tcShortcut.TargetPath) {
+      $tcShortcut.Save()
+    } else {
+      Write-aoWarning ("Cannot create shortcut for '{0}' - target is missing" -f $tcShortcut.TargetPath)
+    } #if
+  } #if
+}
+
+
+$autoexecPath = Join-Path -Path $script:scriptDir -ChildPath "ao-env_autoexec.bat"
 $existingValue = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Command Processor" `
   -Name "AutoRun" -ErrorAction SilentlyContinue
-
 if ($NULL -ne $existingValue) {
   $existingValue = $existingValue.PSObject.Properties.Item("Autorun").Value
   if ($existingValue -ne $autoexecPath) {
@@ -31,4 +56,29 @@ if (Test-Path -Path $envVarsFile) {
   Write-Host ("Creating '{0}' file using default values" -f $envVarsFile)
   Copy-Item -Path (Join-Path -Path $script:scriptDir -ChildPath "set_vars_template.bat") `
     -Destination $envVarsFile
+} #if
+
+$conemuDesktopShortcut = (Join-Path -Path ([Environment]::GetFolderPath("Desktop")) -ChildPath "ConEmu.lnk")
+$conemuConfig = Join-Path -Path $script:scriptDir -ChildPath "local\conemu\ConEmu.xml"
+if (Test-Path -Path $conemuDesktopShortcut) {
+  Write-Host "ConEmu desktop shortcut already exists. Skipping creation"
+} else {
+  if ([Environment]::Is64BitOperatingSystem) {
+    $conemuPath = Join-Path -Path $script:scriptDir -ChildPath "conemu\ConEmu64.exe"
+  } else {
+    $conemuPath = Join-Path -Path $script:scriptDir -ChildPath "conemu\ConEmu.exe"
+  } #if
+  Write-Host "Creating ConEmu desktop shortcut"
+  CreateShortcut -shortcutFileName $conemuDesktopShortcut `
+    -shortcutTarget ('"{0}"' -f $conemuPath) `
+    -arguments @("-loadcfgfile", ('"{0}"' -f $conemuConfig))
+} #if
+
+New-Item -ItemType Directory -Force -Path (Split-Path -Parent $conemuConfig) | Out-Null
+if (Test-Path -Path $conemuConfig) {
+  Write-Host ("'{0}' already exists. Skipping creation" -f $conemuConfig)
+} else {
+  Write-Host ("Creating '{0}' file using default values" -f $conemuConfig)
+  Copy-Item -Path (Join-Path -Path $script:scriptDir -ChildPath "ConEmu_config_template.xml") `
+    -Destination $conemuConfig
 } #if
