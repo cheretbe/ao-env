adbe554e6d4f44b46eca392122f23583dc45f581
cheretbe <a.a.orlov@gmail.com>
2021-06-08 13:59:00 +0200

    Finish 'ca-check-root' script

--- bin/lib/ca-check-root.py
+++ bin/lib/ca-check-root.py
@@ -4,7 +4,6 @@ import os
 import sys
 import argparse
 import pathlib
-import subprocess
 
 sys.path.append(os.path.dirname(__file__))
 import common
@@ -38,21 +37,32 @@ def main():
     root_ca_cert = pathlib.Path(options.ca_path) / "root_ca/root_ca.crt"
     root_ca_key = pathlib.Path(options.ca_path) / "root_ca/root_ca.key"
 
-    # print(common.get_openssl_executable())
-    # subprocess.check_call([common.get_openssl_executable(), "version"])
-    # return
-
-    if not os.path.isfile(root_ca_key):
-        print(f"Root CA key file [{root_ca_key}] is missing")
+    if not root_ca_key.exists():
+        print(f"\nRoot CA key file [{root_ca_key}] is missing")
         common.ask_for_confirmation(
             "Would you like to create it now?",
             batch_mode=options.batch_mode, default=False
         )
         root_ca_key.parent.mkdir(mode=0o775, parents=True, exist_ok=True)
-        subprocess.check_call([
+        common.run([
             common.get_openssl_executable(),
             "genrsa", "-aes256", "-out", root_ca_key, "4096"
         ])
+
+    if not root_ca_cert.exists():
+        print(f"\nRoot CA certificate [{root_ca_cert}] is missing")
+        common.ask_for_confirmation(
+            "Would you like to create it now?",
+            batch_mode=options.batch_mode, default=False
+        )
+        # 7300 days is 20 years
+        common.run(
+            [
+                common.get_openssl_executable(),
+                "req", "-key", root_ca_key, "-new", "-x509", "-days", "7300",
+                "-sha256", "-out", root_ca_cert
+            ] + common.get_openssl_config()
+        )
 
 if __name__ == "__main__":
     main()
--- bin/lib/common.py
+++ bin/lib/common.py
@@ -1,6 +1,7 @@
 import sys
 import platform
 import pathlib
+import subprocess
 import humanfriendly.prompts
 
 def ask_for_confirmation(prompt, batch_mode, default):
@@ -20,8 +21,47 @@ def ask_for_confirmation(prompt, batch_mode, default):
 def get_openssl_executable():
     if sys.platform == "win32":
         cpu_arch = {True: "x86", False: "x64"}[platform.architecture()[0] == "32bit"]
-        return (
+        return str(
             pathlib.Path(__file__).resolve().parents[2] / "windows" / "openssl" /
             cpu_arch / "bin" / "openssl.exe"
         )
     return "openssl"
+
+def get_openssl_config():
+    if sys.platform == "win32":
+        return (
+            [
+                "-config",
+                str(
+                    pathlib.Path(__file__).resolve().parents[2] / "windows" /
+                    "openssl" / "conf" / "openssl.cnf"
+                )
+            ]
+        )
+    return []
+
+def run(cmd_args, echo=True):
+    if echo:
+        print(cmd_args)
+    subprocess.check_call(cmd_args)
+
+def run_with_masked_password(cmd_args, echo=True):
+    cmd_to_run = []
+    cmd_to_print = []
+    for arg in cmd_args:
+        if isinstance(arg, dict):
+            cmd_to_run += [arg["format"].format(arg["password"])]
+            cmd_to_print += [arg["format"].format("*****")]
+        else:
+            cmd_to_run += [arg]
+            cmd_to_print += [arg]
+
+    if echo:
+        print("[{}]".format("], [".join(cmd_to_print)))
+    cmd_rc = subprocess.call(cmd_to_run)
+    if cmd_rc != 0:
+        sys.exit(
+            "Command [{}] returned non-zero exit status {}".format(
+                "], [".join(cmd_to_print), cmd_rc
+            )
+        )
